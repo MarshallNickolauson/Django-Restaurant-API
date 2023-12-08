9e5d37febc79d2b8a33400df922f5724d1df9f01
marsh <marshnickol101>
2023-12-08 06:36:40 -0500

    setting up menu_app

--- django_restaurant_api_project/menu_app/admin.py
+++ django_restaurant_api_project/menu_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Category, MenuItem, Ingredient, MenuItemIngredient
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(MenuItem)
+admin.site.register(Ingredient)
+admin.site.register(MenuItemIngredient)
--- django_restaurant_api_project/menu_app/models.py
+++ django_restaurant_api_project/menu_app/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to='category_images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -13,7 +12,6 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    image = models.ImageField(upload_to='item_images/', null=True, blank=True)
     ingredients = models.ManyToManyField('Ingredient', through='MenuItemIngredient')
 
     def __str__(self):
